4d41d48abd648932a3017551295d8eb346c44259
Joe Bruzek <jbruzek@Joes-MacBook-Pro.local>
2014-10-12 00:35:59 -0400

    changing Memman to SearchTree

---
+++ src/SearchTree.java
@@ -0,0 +1,239 @@
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Main SearchTree class for the project. Reads from file and executes commands
+ *
+ * @author jbruzek, sucram20
+ *
+ */
+public class SearchTree {
+
+    // On my honor:
+    //
+    // - I have not used source code obtained from another student,
+    // or any other unauthorized source, either modified or
+    // unmodified.
+    //
+    // - All source code and documentation used in my program is
+    // either my original work, or was derived by me from the
+    // source code published in the textbook for this course.
+    //
+    // - I have not discussed coding details about this project with
+    // anyone other than my partner (in the case of a joint
+    // submission), instructor, ACM/UPE tutors or the TAs assigned
+    // to this course. I understand that I may discuss the concepts
+    // of this program with other students, and that another student
+    // may help me debug my program so long as neither of us writes
+    // anything during the discussion or modifies any computer file
+    // during the discussion. I have violated neither the spirit nor
+    // letter of this restriction.
+
+    /**
+     * the main function for the program. Takes arguments.
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        int hashSize = 0;
+        int blockSize = 0;
+        File file;
+        HashTable artists;
+        HashTable songs;
+        Tree tree;
+
+        hashSize = Integer.parseInt(args[0]);
+        blockSize = Integer.parseInt(args[1]);
+        file = new File(args[2]);
+        artists = new HashTable(hashSize, "Artist");
+        songs = new HashTable(hashSize, "Song");
+        tree = new Tree();
+
+        MemoryManager mm = new MemoryManager(blockSize);
+
+        FileReader reader = new FileReader(file);
+
+        executeCommands(reader.getCommands(), mm, artists, songs, tree);
+    }
+
+    /**
+     * execute all the commands grabbed from the filereader
+     * @param commands
+     * @param mm
+     * @param artists
+     * @param songs
+     */
+    public static void executeCommands(ArrayList<String> commands, MemoryManager mm, HashTable artists, HashTable songs, Tree tree) {
+        // run every command in the commands list
+        for (int i = 0; i < commands.size(); i++) {
+            String command = commands.get(i);
+            String[] tokens = command.split(" ");
+
+            //get the command (key) and the parameter (value)
+            String key = command.substring(0, tokens[0].length());
+            String value = command.substring(key.length() + 1, command.length());
+
+            //process the command
+            switch(key) {
+                case "insert":
+                    Pair<String, String> sPair = toPair(value);
+                    Pair<byte[], byte[]> bPair = toByteArray(sPair);
+                    MemHandle h1;
+                    MemHandle h2;
+                    if (artists.duplicate(sPair.getFirst())) {
+                        h1 = mm.insert(bPair.getFirst());
+                        artists.hashInsert(sPair.getFirst(), h1);
+                        System.out.println("|" + sPair.getFirst() + "| is added to the artist database.");
+                    } else {
+                        System.out.println("|" + sPair.getFirst() + "| duplicates a record already in the artist database.");
+                    }
+                    if (songs.duplicate(sPair.getSecond())) {
+                        h2 = mm.insert(bPair.getSecond());
+                        songs.hashInsert(sPair.getSecond(), h2);
+                        mm.increased();
+                        System.out.println("|" + sPair.getSecond() + "| is added to the song database.");
+                    } else {
+                        System.out.println("|" + sPair.getSecond() + "| duplicates a record already in the song database.");
+                    }
+                    KVPair p1 = new KVPair(h1, h2);
+                    KVPair p2 = new KVPair(h2, h1);
+                    tree.add(p1);
+                    System.out.println("The KVPair (|" + sPair.getFirst() + "|,|" + sPair.getSecond() 
+                    		+ "|),(" + h1.getPosition() + "," + h2.getPosition() + ") is added to the tree.");
+                    tree.add(p2);
+                    System.out.println("The KVPair (|" + sPair.getSecond() + "|,|" + sPair.getFirst() 
+                    		+ "|),(" + h2.getPosition() + "," + h1.getPosition() + ") is added to the tree.");
+
+                    break;
+                case "remove":
+                    String[] words = value.split(" ");
+                    String type = words[0];
+                    String name = value.substring(type.length() + 1, value.length());
+
+                    switch(type) {
+                        case "artist":
+                            if (artists.searchTable(name)) {
+                                MemHandle rh = artists.hashDelete(name);
+                                mm.release(rh);
+                                System.out.println("|" + name + "| is removed from the artist database.");
+                            } else {
+                                System.out.println("|" + name + "| does not exist in the artist database.");
+                            }
+                            break;
+                        case "song":
+                            if (songs.searchTable(name)) {
+                                MemHandle rh = songs.hashDelete(name);
+                                mm.release(rh);
+                                System.out.println("|" + name + "| is removed from the song database.");
+                            } else {
+                                System.out.println("|" + name + "| does not exist in the song database.");
+                            }
+                            break;
+                        default:
+                            //nothing
+                    }
+                    break;
+                case "print":
+                    switch(value) {
+                        case "blocks":
+                            System.out.println(mm.getBlocks());
+                            break;
+                        case "artist":
+                            Pair<MemHandle[], int[]> pair = artists.findAll();
+                            String[] names = mm.getNames(pair.getFirst());
+                            for (int j = 0; j < names.length; j++) {
+                                System.out.println("|" + names[j] + "| " + pair.getSecond()[j]);
+                            }
+                            System.out.println("total artists: " + names.length);
+                            break;
+                        case "song":
+                            Pair<MemHandle[], int[]> pair2 = songs.findAll();
+                            String[] names2 = mm.getNames(pair2.getFirst());
+                            for (int j = 0; j < names2.length; j++) {
+                                System.out.println("|" + names2[j] + "| " + pair2.getSecond()[j]);
+                            }
+                            System.out.println("total songs: " + names2.length);
+                            break;
+                        default:
+                            //nothing
+                    }
+                    break;
+                default:
+                    //nothing was there
+            }
+        }
+    }
+
+    /**
+     * get a pair of strings from a string with a <SEP> marker
+     * @param str
+     * @return
+     */
+    public static Pair<String, String> toPair(String str) {
+        Pair<String, String> pair = new Pair<String, String>();
+        String[] tokens = str.split("<SEP>");
+        String first = tokens[0];
+        String second = tokens[1];
+
+        pair.setFirst(first);
+        pair.setSecond(second);
+
+        return pair;
+    }
+
+    /**
+     * get a byte array pair from a String pair
+     * @param str
+     * @return
+     */
+    public static Pair<byte[], byte[]> toByteArray(Pair<String, String> str) {
+        Pair<byte[], byte[]> pair = new Pair<byte[], byte[]>();
+
+        //Set the Artist
+        char[] s = str.getFirst().toCharArray();
+        byte[] bytes = getBytes(str.getFirst());
+
+        for (int i = 0; i < s.length; i++) {
+            bytes[i + 2] = (byte) s[i];
+        }
+
+        pair.setFirst(bytes);
+
+        //Set the Song title
+        char[] f = str.getSecond().toCharArray();
+        byte[] bites = getBytes(str.getSecond());
+
+        for (int i = 0; i < f.length; i++) {
+            bites[i + 2] = (byte) f[i];
+        }
+
+        pair.setSecond(bites);
+
+        return pair;
+    }
+
+    /**
+     * get the byte array for a string, encoding the length into the first two bytes
+     * @param str
+     * @return
+     */
+    public static byte[] getBytes(String str) {
+        int length = str.length();
+        byte[] data = new byte[length + 2];
+        data[0] = (byte) (length & 0xFF);
+        data[1] = (byte) ((length >> 8) & 0xFF);
+
+        return data;
+    }
+
+    /**
+     * get the length from the first two bytes of the byte array
+     * @param data
+     * @return
+     */
+    public static int getLength(byte[] data)
+    {
+       return (int)((data[1]<<8) | (data[0]));
+    }
+
+}
---
+++ src/SearchTreeTest.java
@@ -0,0 +1,98 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import junit.framework.TestCase;
+
+/**
+ * test the SearchTree class
+ * @author jbruzek, sucram20
+ *
+ */
+public class SearchTreeTest extends TestCase {
+	
+	private ArrayList<String> commands;
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	
+	/**
+	 * set up before tests
+	 */
+	public void setUp() {
+		System.setOut(new PrintStream(outContent));
+		commands = new ArrayList<String>();
+		commands.add("insert this<SEP>that");
+		commands.add("remove artist this");
+		commands.add("remove song that");
+		commands.add("remove artist not there");
+		commands.add("remove song not there");
+		commands.add("print blocks");
+		commands.add("print artist");
+		commands.add("print song");
+		commands.add("jsdhbjsdavjdf jbkvsa");
+	}
+	
+	/**
+	 * test main
+	 */
+	public void testMain() {
+		String[] str = new String[3];
+		str[0] = "10";
+		str[1] = "32";
+		str[2] = "src/test1.txt";
+		SearchTree.main(str);
+		
+		assertTrue(!outContent.toString().equals(""));
+	}
+   
+	/**
+	 * test the get bytes method
+	 */
+    public void testGetBytes() {
+    	String test = "Length: 10";
+    	
+    	byte[] bytes = SearchTree.getBytes(test);
+    	assertEquals(12, bytes.length);
+    	assertEquals((byte) 10, bytes[0]);
+    	assertEquals((byte) 0, bytes[1]);
+    }
+    
+    /**
+     * test toPair method
+     */
+    public void testToPair() {
+    	String str = "First thing<SEP>Second thing";
+    	Pair<String, String> p = SearchTree.toPair(str);
+    	
+    	assertEquals("First thing", p.getFirst());
+    	assertEquals("Second thing", p.getSecond());
+    }
+    
+    /**
+     * test the toByteArray method
+     * 
+     * implicitely test the getLength method
+     */
+    public void testToByteArray() {
+    	String str = "Length: 10<SEP>Length: 15-----";
+    	Pair<String, String> p = SearchTree.toPair(str);
+    	
+    	Pair<byte[], byte[]> pair = SearchTree.toByteArray(p);
+    	
+    	assertEquals(10, SearchTree.getLength(pair.getFirst()));
+    	assertEquals(15, SearchTree.getLength(pair.getSecond()));
+    }
+    
+	/**
+     * test the execute commands method
+     */
+    public void testExecuteCommands() {
+    	MemoryManager mm = new MemoryManager(1000);
+    	HashTable a = new HashTable(10, "Artists");
+    	HashTable b = new HashTable(10, "Songs");
+    	Tree t = new Tree();
+    	SearchTree.executeCommands(commands, mm, a, b, t);
+    	
+    	assertEquals(1, mm.getBlocks().size());
+    	assertEquals(10, a.getSize());
+    	assertEquals(10, b.getSize());
+    }
+}
